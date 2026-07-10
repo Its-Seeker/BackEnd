4dd67f21e36b75c32e904a783bd93fc9b37ac24c
Its-Seeker <ketanarora482@gmail.com>
2026-07-10 13:33:08 +0530

    Update app.js

--- Express Program/app.js
+++ Express Program/app.js
@@ -10,8 +10,8 @@ app.get("/about", (req,res)=> res.send("<h1>Hello about page!</h1>"));
 app.get("/contact", (req,res)=>{
  return res.send(`const team = [
   {
-    name: "Anshul",
-    role: "Frontend Developer",
+    name: "Ketan",
+    role: "React js Developer",
     desc: "Builds modern, responsive UI using React.",
     image: ketanImg,
     linkedin: "https://www.linkedin.com/in/ketan-arora-028961325/",
