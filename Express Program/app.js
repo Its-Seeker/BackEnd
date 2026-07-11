50882c01df801112e0d3d1b3e0135f4273b57277
Its-Seeker <ketanarora482@gmail.com>
2026-07-12 02:24:51 +0530

    How to make port with various methods

--- Express Program/app.js
+++ Express Program/app.js
@@ -3,25 +3,27 @@ import {PORT} from "./env.js"
 
 const app = express ();
 
-app.get("/", (req,res)=> res.send("<h1> Hello World! </h1>"));
+// app.get("/", (req,res)=> res.send("<h1> Hello World! </h1>"));
 
-app.get("/about", (req,res)=> res.send("<h1>Hello about page!</h1>"));
+// app.get("/about", (req,res)=> res.send("<h1>Hello about page!</h1>"));
 
 
-app.get("/contact", (req,res)=>{
- return res.send(`const team = [
-  {
-    name: "Ketan",
-    role: "React js Developer",
-    desc: "Builds modern, responsive UI using React.",
-    image: ketanImg,
-    linkedin: "https://www.linkedin.com/in/ketan-arora-028961325/",
-    github: "https://github.com/Its-Seeker",
-  },`)});
+// app.get("/contact", (req,res)=>{
+//  return res.send(`const team = [
+//   {
+//     name: "Ketan",
+//     role: "React js Developer",
+//     desc: "Builds modern, responsive UI using React.",
+//     image: ketanImg,
+//     linkedin: "https://www.linkedin.com/in/ketan-arora-028961325/",
+//     github: "https://github.com/Its-Seeker",
+//   },`)});
 
 
 // const PORT = process.env.PORT || 3000;
 app.listen(PORT , ()=>{
-  console.log(`Server is running at port : ${PORT}`);
+  // console.log(`Server is running at port : ${PORT}`);
+  console.log("server is running at port 3000");
+  
   
 });
